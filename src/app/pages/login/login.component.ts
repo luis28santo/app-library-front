b2b2cdee72ebb69b9c37e285f2911d8ad0d1b4f6
Luis Angel SM <luis28santo@gmail.com>
2025-12-02 21:42:06 -0500

    first commit

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  private router = inject(Router);
+
+  login(): void {
+    this.router.navigate(['/home']);
+  }
+}
